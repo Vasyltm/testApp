550adc3fff94dac36e94bb9cb26f494b4531e07d
Vasyl <vasultm@yahoo.com>
2020-07-01 22:35:38 +0300

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Vasyl Travlinskyy on 01.07.2020.
+//  Copyright © 2020 Vasyl Travlinskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
